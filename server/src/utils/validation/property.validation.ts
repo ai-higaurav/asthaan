cc760f2e5e3a62940565638c8170725afa104df6
ai-higaurav <ai.higaurav@gmail.com>
2024-06-22 09:17:28 +0530

    property validation added

--- server/src/utils/validation/property.validation.ts
+++ server/src/utils/validation/property.validation.ts
@@ -1,149 +1,146 @@
-import { z } from 'zod';
+import mongoose from 'mongoose';
+import {z} from 'zod'
 
-const listingTypeOptions = ['sale', 'rent', 'lease'];
-const propertyTypeOptions=['apartment', 'house', 'villa', 'commercial space'];
-const buildingTypeOptions=['residential', 'commercial', 'mixed-use'];
-const possessionStatusOptions=["ready to move"," under construction"];
-const balconyTypeOptions=['none', 'open', 'covered']
-const amenitiesOptions=['swimming pool', 'gym', 'park', 'security', 'playground', 'club house']
-const waterSourceOptons=['municipal corporation', 'borewell', 'tank']
-const facingDirectionOptions=['north', 'south', 'east', 'west', 'north-east', 'north-west', 'south-east', 'south-west']
-const furnishedStatusOptions=['furnished','semi-furnished','unfurnished']
+const listingTypeEnum = ["sale"];
 
+const propertyTypeEnum = ["apartment", "house", "villa"];
 
+const buildingTypeEnum = ["residential", "commercial"];
+
+const furnishedStatusEnum = ["furnished", "semi furnished", "full furnished"];
+
+const possessionStatusEnum =["ready to move", "under constraction"];
+
+const waterSourceEnum = ['municipal corporation', 'borewell', 'tank'];
+
+const facingDirectionEnum = ['north', 'south', 'east', 'west', 'north-east', 'north-west', 'south-east', 'south-west'];
+
+const balconyTypeEnum = ["open","closed","covered"]
+
+const amenitiesEnum=['swimming pool', 'gym', 'park', 'security', 'playground', 'club house']
+
+// const statusEnum = ["active", "sold"];
 
 const propertyVal = z.object({
-  listingType: z
+    listingType:z
     .string({
-      required_error: "Listing type is required!",
-      invalid_type_error: "Listing type must be a string",
+        required_error:"Listing type is required",
+        invalid_type_error:"Listing type must be a string"
     })
-    .transform((val)=> val.toLocaleLowerCase())
-    .refine((val) => listingTypeOptions.includes(val), {
-      message: "Invalid listing type. Must be 'Sale', 'Rent', or 'Lease'.",
-    }),
+    .transform((val)=> val.toLowerCase())
+    .refine((val)=> listingTypeEnum.includes(val),{message:"Invalid listing type. Must be 'Sale'. "}),
 
-  propertyType: z.string({
-    required_error: "Property type is required!",
-    invalid_type_error: "Property type must be a string",
-  })
-  .transform((val)=> val.toLocaleLowerCase())
-  .refine((val) => propertyTypeOptions.includes(val), {
-    message: "Invalid Property type. Must be 'apartment', 'house', 'villa' , 'commercial space'.",
-  }),
+    propertyType:z
+    .string({
+        required_error:"Property type is required",
+        invalid_type_error:"Property type must be a string"
+    })
+    .transform((val)=> val.toLowerCase())
+    .refine((val)=> propertyTypeEnum.includes(val),{message:"Invalid property type. Must be apartment , house or villa "}),
 
-  buildingType:z
-  .string({
-    required_error:'Building type is required',
-    invalid_type_error:'Building type must be a string'
-  })
-  .transform((val)=> val.toLowerCase())
-  .refine(val => buildingTypeOptions.includes(val),{
-    message:"Invalid building type. Must be 'residential', 'commercial'" 
-  }),
+    buildingType:z
+    .string({
+        required_error:"Building type is required",
+        invalid_type_error:"Building type must be a string"
+    })
+    .transform((val)=> val.toLowerCase())
+    .refine((val)=> buildingTypeEnum.includes(val),{message:"Invalid building type. Must be residential or commercial"}),
 
-  furnishedStatus:z
-  .string({
-    required_error:'Furnished type is required',
-    invalid_type_error:'Furnished type must be a string'
-  })
-  .transform((val)=> val.toLowerCase())
-  .refine(val => furnishedStatusOptions.includes(val),{
-    message:"Invalid furnished type. Must be 'furnished', 'semi-furnished','unfurnished'" 
-  }),
+    furnishedStatus:z
+    .string({
+        required_error:"Furnished status is required",
+        invalid_type_error:"Furnished status must be a string"
+    })
+    .transform((val)=> val.toLowerCase())
+    .refine((val)=> furnishedStatusEnum.includes(val),{message:"Invalid furnished status be furnished , semi-furnished or full furnished"}),
+
+    possessionStatus:z
+    .string({
+        required_error:"Possession status is required",
+        invalid_type_error:"Possession status must be a string"
+    })
+    .transform((val)=> val.toLowerCase())
+    .refine((val)=> possessionStatusEnum.includes(val),{message:"Possession must be ready to move or under constraction"}),
+
+    waterSource:z
+    .string({
+        required_error:"Water source is required",
+        invalid_type_error:"Water source must be a string"
+    })
+    .transform((val)=> val.toLowerCase())
+    .refine((val)=> waterSourceEnum.includes(val),{message:"Water source must be muncipal , borewell or tank"}),
+
+   facingDirection:z
+   .string({
+    required_error:"Facing direction is required",
+    invalid_type_error:"Facing direction must be a string"
+   })
+   .transform((val)=> val.toLowerCase())
+   .refine((val)=> facingDirectionEnum.includes(val),{message:"Facing direction must be valid"}),
 
   propertyAge:z
   .number({
-    required_error:'Property age is required',
-    invalid_type_error:'Property age must be  number'
+    required_error:"Property age is required",
+    invalid_type_error:"Property age must be a number"
   })
-  .min(1,"Minimum age property must a 0-1")
-  .max(100, "Maximum age property is 100 years"),
-
-  totalFloors:z
-  .number({
-    required_error:'Total No of floor is  required',
-    invalid_type_error:'Total Floor must be a number'
-  })
-  .min(1,"Floor must be more than zero")
-  .max(100, "Maximum floor is 100"),
-
-  floorNo:z
-  .number({
-    required_error:'Floor No  is  required',
-    invalid_type_error:'Floor must be a number'
-  })
-  .min(1,"Floor must be more than zero")
-  .max(100, "Maximum floor is 100"),
-
-  possessionStatus:z
-  .string({
-    required_error:'Possession is required',
-    invalid_type_error:'Possession must be a string'
-  })
-  .transform(val=> val.toLowerCase())
-  .refine(val => possessionStatusOptions.includes(val),{
-    message:"Possession status must be 'ready to move or under construction"
-  }),
+  .min(1, "Property number must be 1 if 0-1 years old")
+  .max(100, "Property maximum age is 100 years"),
 
   unitNo:z
   .number({
-    invalid_type_error:'Unit number must be a number',
-    required_error:'unit number is required'
+    required_error:"Unit number  is required",
+    invalid_type_error:"Unit Number  must be a number"
   })
-  .min(0,"Minimum Unit is required"),
+  .min(1, "Minimum unit should be 1")
+  .max(100, "Maximum unit limit is 50"),
 
   rooms:z
   .number({
-    required_error:'No of rooms is required',
-    invalid_type_error:'No of rooms must be a number'
-  }),
+    required_error:"No of rooms  is required",
+    invalid_type_error:"No of rooms  must be a number"
+  })
+  .min(1, "Minimum 1 room is required")
+  .max(100, "Maximum room limit is 100"),
 
   bathroom:z
   .number({
-    required_error:'No of bathroom is required',
-    invalid_type_error:'No of bathroom must be a number'
-  }),
-
-  view:z
-  .string({
-    required_error:'Property view is required',
-    invalid_type_error:'Property view must be a string'
-  }),
+    required_error:"No of bathroom is required",
+    invalid_type_error:"No of bathroom  must be a number"
+  })
+  .min(1, "Minimum 1 bathroom is required")
+  .max(100, "Maximum bathroom limit is 100"),
 
   area:z
   .number({
-    required_error:'area is required',
-    invalid_type_error:'area must be a number'
-  }),
+    required_error:"Area is required",
+    invalid_type_error:"Area must be a number"
+  })
+  .min(1, "Minimum 1 SqFt. area is required")
+  .max(100, "Maximum SqFt. area is 1000 SqFt."),
 
   price:z
   .number({
-    required_error:'price is required',
-    invalid_type_error:'price  must be a number'
-  }),
+    required_error:"Price is required",
+    invalid_type_error:"Price must be a number"
+  })
+  .min(1, "Price must be more than 1 Rs")
+  .max(10000000 , "Maximum price limit is 1 Cr"),
 
-  city:z
-  .string({
-    required_error:'city is required',
-    invalid_type_error:'city must be a string'
-  }),
-
-  locality:z
-  .string({
-    required_error:'locality is required',
-    invalid_type_error:'locality must be a string'
-  }),
+  totalFloor:z
+  .number({
+    required_error:"Total no of is required",
+    invalid_type_error:"Total no of floor must be a number"
+  })
+  .min(1, "Minimum 1 floor is required")
+  .max(100, "Maximum floor limit is 100"),
 
   balconyType:z
   .string({
-    required_error:'Balcony type is required',
-    invalid_type_error:'Balcony type must be a string'
+    required_error:"Balcony type is required",
+    invalid_type_error:"Balcony must be a string"
   })
-  .transform(val=> val.toLowerCase())
-  .refine(val => balconyTypeOptions.includes(val),{
-    message:"Balconoy type must be 'none, open or covered"
-  }),
+  .transform((val)=> val.toLowerCase())
+  .refine((val)=> balconyTypeEnum.includes(val),{message:"Balcony must be open , covered or none"}),
 
   amenities: z.array(
     z.string({
@@ -151,42 +148,55 @@ const propertyVal = z.object({
         invalid_type_error: 'Each amenity must be a string'
     })
     .transform(val => val.toLowerCase())
-    .refine(val => amenitiesOptions.includes(val), {
+    .refine(val => amenitiesEnum.includes(val), {
         message: "Each amenity must be one of the following: 'swimming pool', 'gym', 'park', 'security', 'playground', 'club house'"
-    })
-),
-
-  waterSource:z
-  .string({
-    required_error:'Water source  is required',
-    invalid_type_error:'Water souce  must be a string'
-  })
-  .transform(val=> val.toLowerCase())
-  .refine(val => waterSourceOptons.includes(val),{
-    message:"Water Souce  must be 'borewell , tank etc.."
-  }),
-
-  facingDirection:z
-  .string({
-    required_error:'FaciFacing direction must be a string'
-  })
-  .transform(val=> val.toLowerCase())
-  .refine(val => facingDirectionOptions.includes(val),{
-    message:"Facing direction  must be 'east, west etc.."
-  }),
+    })),
 
   additionalDetails:z
   .string({
     required_error:'Property discription is required',
-    invalid_type_error:'Property must be a string'
+    invalid_type_error:'Property discription must be a string'
   }),
 
-  seller:z
+  state:z
   .string({
-    invalid_type_error:'Seller must be a string',
-    required_error:'Seller id is required'
-  })  
+    required_error:'State is required',
+    invalid_type_error:"State must be a string",
+  })
+  .transform((val)=> val.toLowerCase()),
 
-});
+  city:z
+  .string({
+    required_error:'City is required',
+    invalid_type_error:"City must be a string"
+  })
+  .transform((val)=> val.toLowerCase()),
+
+  locality:z
+  .string({
+    required_error:'Locality is required',
+    invalid_type_error:"Locality must be a string"
+  })
+  .transform((val)=> val.toLowerCase()),
+
+  pinCode:z
+  .number({
+    required_error:'Pincode is required',
+    invalid_type_error:"Pincode must be a number"
+  })
+  .int()
+  .gte(100000)
+  .lte(999999)
+  .refine((val)=> /^\d{6}$/g.test(val.toString()),{message:"Pin code must be 6 digit"}),
+
+//   seller:z
+//   .string({
+//     required_error:'Seller id is required',
+//     invalid_type_error:"Seller id must a valid object id"
+//   })
+//   .refine((id)=>mongoose.Types.ObjectId.isValid(id),{message:"Seller id must be a valid id"})
+
+})
+
 
 export default propertyVal;
